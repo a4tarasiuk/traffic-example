628de91ab1ff977b8d9413f15fff76f896e2ecbb
a4tarasiuk <>
2024-08-16 13:48:15 +0300

    Add main dependencies

---
+++ tra/apps/forecast_rules/domain/enums.py
@@ -0,0 +1,9 @@
+from enum import IntEnum
+
+
+class ForecastModelEnum(IntEnum):
+    SIMPLE = 1
+
+
+class ForecastDistributionModelEnum(IntEnum):
+    SIMPLE = 1
---
+++ tra/core/enums.py
@@ -0,0 +1,6 @@
+from enum import IntEnum
+
+
+class DirectionEnum(IntEnum):
+    IN = 1
+    OUT = 2
